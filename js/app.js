967a91b4f68483a307dcdf33f9c8161858e3fe23
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-02-02 17:37:58 +0100

    EDIT for cicle

--- js/app.js
+++ js/app.js
@@ -4,19 +4,16 @@ for (let i = 1; i <= 100; i++) {
     const multi5 = i % 5;
     const multi15 = i % 15;
 
-if (multi3 === 0){
+if (multi15 === 0){
+    console.log('BuzzFizz')
+
+}else if (multi5 === 0){
+    console.log('Fizz')
+
+}else if (multi3 === 0){
     console.log('Buzz')
 
 }else console.log(i)
 
-if (multi5 === 0){
-    console.log('Fizz')
-    
-}else console.log(i)
-
-
-if(multi15 === 0){
-    console.log('BuzzFizz')
-
-}else console.log(i)
 }
+
