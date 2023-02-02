10d0529eab2beb71de104d870a11b3b138956f2c
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-02-02 15:55:40 +0100

    EDIT for cicle

---
+++ js/app.js
@@ -0,0 +1,30 @@
+for (let i = 0; i < 100; i++) {
+
+	const multi15 = i % 15
+
+	console.log(i, multi15)
+
+	if (i === 3) {
+		console.log('Fizz')
+
+	} else if (i === 5) {
+		console.log('buzz')
+
+	} else if (i == 6){
+		console.log('Fizz')
+
+    } else if(i === 9){
+		console.log('Fizz')
+
+    } else if (i === 10) {
+		console.log('buzz')
+
+	} else if(i === 12); {
+		console.log('Fizz')
+    
+    } else (i === 0);{
+        console.log('BuzzFizz')
+    }
+    
+    
+}
