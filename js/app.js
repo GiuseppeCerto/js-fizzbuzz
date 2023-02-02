076dbb13e05380dc644b5fb008c50d8228219acc
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-02-02 17:05:15 +0100

    EDIT try to

--- js/app.js
+++ js/app.js
@@ -1,30 +1,22 @@
-for (let i = 0; i < 100; i++) {
+for (let i = 1; i <= 100; i++) {
 
-	const multi15 = i % 15
+	const multi3 = i % 3;
+    const multi5 = i % 5;
+    const multi15 = i % 15;
 
-	console.log(i, multi15)
+if (multi3 === 0){
+    console.log('Buzz')
 
-	if (i === 3) {
-		console.log('Fizz')
+}else console.log(i)
 
-	} else if (i === 5) {
-		console.log('buzz')
-
-	} else if (i == 6){
-		console.log('Fizz')
-
-    } else if(i === 9){
-		console.log('Fizz')
-
-    } else if (i === 10) {
-		console.log('buzz')
-
-	} else if(i === 12); {
-		console.log('Fizz')
+if (multi5 === 0){
+    console.log('Fizz')
     
-    } else (i === 0);{
-        console.log('BuzzFizz')
-    }
-    
-    
+}else console.log(i)
+
+
+if(multi15 === 0){
+    console.log('BuzzFizz')
+
+}else console.log(i)
 }
